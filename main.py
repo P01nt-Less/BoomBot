3e249ff4515673bcf9b92843a0902e969e92958e
Pointless <MindScriptMC@gmail.com>
2018-06-29 23:32:49 +0100

    oof

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-# Discord Imports
+#Discord Imports
 import discord
 from discord.ext import commands
 import discord.utils
-# Python Imports
-import re
-import os
-# Other Imports
-prefix=commands.when_mentioned_or('!')
-bot = commands.Bot(command_prefix=prefix, description='The one, and only: The Bot, created by Pointless#1278.', self_bot=False,)
+#Other Imports
+import traceback #to see the error while in Discord
+import random # for randomizing things
+from random import randint # randints
+import requests # for APIs
+import aiohttp # for APIs 2
+import sys #for system-specific parameters and functions
+import re # for regular expression operations
+import json # self-explanatory kek
+import time # also self-explanatory le
+import asyncio #tasks and such
+import os # operating system stuff and heroku
+import datetime # self-explanatory too lol
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='The one, and only: The Bot, created by Pointless#1278.', self_bot=False,)
 bot.remove_command('help')
+
+start_time = time.time()
+starttime2 = time.ctime(int(time.time()))
 
 def pointcheck(ctx):
     return ctx.message.author.id == '276043503514025984' #checks if @Pointless#1278 is the author of the command
@@ -19,13 +31,6 @@ async def on_ready():
     print('Online.')
     print('https://discordapp.com/oauth2/authorize?client_id=431951773159129098&scope=bot&permissions=2146958591')
   
-@bot.event
-async def on_message(message):
-    if message.content.startswith(str(prefix) + str('test')):
-        bot.say('Hey!')
-    else:
-        bot.say('Oh boy!')
-
-if 'TOKEN' in os.environ:
-    heroku = True
-    TOKEN = os.environ['TOKEN']
+if not os.environ.get('TOKEN'):
+   print("No tokens!")
+bot.run(os.environ.get('TOKEN')
